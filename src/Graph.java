263ebf4eca54091bb3ac254847c36a6ddcf38b6d
Anderson Freire Flores <andersonfflores75@gmail.com>
2023-12-11 15:27:09 -0300

    Versão Da entrega

--- src/Graph.java
+++ src/Graph.java
@@ -9,14 +9,12 @@ public class Graph {
 
     private int numVertices;
 
-    // Initialize the matrix
     public Graph(int numVertices) {
         this.numVertices = numVertices;
         adjMatrix = new boolean[numVertices][numVertices];
         matrixDistancia = new double[numVertices][numVertices];
     }
 
-    // Add edges
     public void addEdge(int i, int j, double distancia) {
         adjMatrix[i][j] = true;
         adjMatrix[j][i] = true;
@@ -24,13 +22,14 @@ public class Graph {
         matrixDistancia[j][i] = distancia;
     }
 
-    // Remove edges
     public void removeEdge(int i, int j) {
         adjMatrix[i][j] = false;
         adjMatrix[j][i] = false;
+        matrixDistancia[i][j] = 0;
+        matrixDistancia[j][i] = 0;
+
     }
 
-    // Print the matrix
     public String toString() {
         StringBuilder s = new StringBuilder();
         for (int i = 0; i < numVertices; i++) {
@@ -74,15 +73,17 @@ public class Graph {
         if (origem == destino){
             return "A origem e o destino são o mesmo lugar!";
         }else {
-        resultado.append("O menor caminho de ").append(Cidades.getCidade(origem)).append(" para ").append(Cidades.getCidade(destino)).append(" é: ").append(distancias[destino]).append("\n").append("Caminho: ");
+        resultado.append("O menor caminho de ").append(Cidades.getCidade(origem)).append(" para ").append(Cidades.getCidade(destino)).append(" é: ")
+                .append(distancias[destino]);
+                /*.append("\n").append("Caminho: ");
 
             Set<String> antiRepetição = new TreeSet<>();
-            for (Integer predecessore : predecessores) {
-                antiRepetição.add("\n ->" + Cidades.getCidade(predecessore));
+            for (Integer predecessoresFor : predecessores) {
+                antiRepetição.add("\n ->" + Cidades.getCidade(predecessoresFor));
             }
             for(String x : antiRepetição){
                 resultado.append(x);
-            }
+            }*/
         }
 
         return resultado.toString();
